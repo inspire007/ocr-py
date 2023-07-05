5c2f8be0eafed0e12aedbf98efe34ec7d941038c
inspire007 <mailindigo734@gmail.com>
2023-07-05 19:00:55 +0600

    Initial commit

---
+++ ocr.py
@@ -0,0 +1,20 @@
+import os
+
+DATA_DIR = os.getcwd() + '/data/'
+
+
+def get_training_data():
+	pass
+
+def read_file_bytes(filename):
+	with open(filename, 'r') as file:
+		pass
+
+	return
+
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
